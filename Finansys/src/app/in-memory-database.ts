c7b8d0fa7cade2c99f0f647602eb042c31113be9
Welton Castoldi <weltoncastoldi@hotmail.com>
2019-07-04 14:22:03 -0300

    adicao do category service

--- Finansys/src/app/in-memory-database.ts
+++ Finansys/src/app/in-memory-database.ts
@@ -1,11 +1,13 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import {Category} from './pages/categories/shared/category.model';
+
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
-            { id: 1, nome: 'Moradia', description: 'Pagamentos de contas de casa'},
-            { id: 2, nome: 'Saúde', description: 'PLano de saúde e remédios'},
-            { id: 3, nome: 'Lazer', description: 'Cinema, parques etc'}
+        const categories: Category[] = [
+            { id: 1, name: 'Moradia', description: 'Pagamentos de contas de casa'},
+            { id: 2, name: 'Saúde', description: 'PLano de saúde e remédios'},
+            { id: 3, name: 'Lazer', description: 'Cinema, parques etc'}
         ];
 
         return {categories};
